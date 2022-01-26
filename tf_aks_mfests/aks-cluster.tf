f1c70de9c885c4fab8053dff4e5490ecd457667f
scoobyboy <paulrichards20@msn.com>
2022-01-26 13:14:35 +0000

    h

--- tf_aks_mfests/aks-cluster.tf
+++ tf_aks_mfests/aks-cluster.tf
@@ -1,29 +1,3 @@
-# Provision AKS Cluster
-/*
-1. Add Basic Cluster Settings
-  - Get Latest Kubernetes Version from datasource (kubernetes_version)
-  - Add Node Resource Group (node_resource_group)
-2. Add Default Node Pool Settings
-  - orchestrator_version (latest kubernetes version using datasource)
-  - availability_zones
-  - enable_auto_scaling
-  - max_count, min_count
-  - os_disk_size_gb
-  - type
-  - node_labels
-  - tags
-3. Enable MSI
-4. Add On Profiles 
-  - Azure Policy
-  - Azure Monitor (Reference Log Analytics Workspace id)
-5. RBAC & Azure AD Integration
-6. Admin Profiles
-  - Windows Admin Profile
-  - Linux Profile
-7. Network Profile
-8. Cluster Tags  
-*/
-
 resource "azurerm_kubernetes_cluster" "hw_aks_cluster" {
   name                = "${azurerm_resource_group.rg_hw.name}-cluster01"
   location            = azurerm_resource_group.rg_hw.location
@@ -53,26 +27,24 @@ resource "azurerm_kubernetes_cluster" "hw_aks_cluster" {
       "environment"      = "dev"
       "nodepoolos"       = "linux"
       "app"              = "system-apps" 
-   } 
-  }
-
+   }
+  
+}
 # Identity (System Assigned or Service Principal)
   identity {
     type = "SystemAssigned"
   }
-}
+
   # Linux Profile
 linux_profile {
     admin_username = "ubuntu"
     #ssh_key {
     #key_data = file(var.ssh_public_key )
     }
-}
+
  # Network Profile
-   network_profile  {
+network_profile  {
     network_plugin = "azure"
     load_balancer_sku = "Standard"
-   }
 }
-
-
+} 
