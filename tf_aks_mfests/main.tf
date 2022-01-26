e9ca0657d69aa5e64190ab4587b80c4cbc52a704
scoobyboy <paulrichards20@msn.com>
2022-01-26 14:11:45 +0000

    j

--- tf_aks_mfests/main.tf
+++ tf_aks_mfests/main.tf
@@ -2,13 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=2.75.0"
+      version = "=2.91.0"
     }
-  }
-}
-
-terraform {
-  backend "azurerm" {
   }
 }
 
