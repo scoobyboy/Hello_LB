b59b20a756efa377510a98c5419e6b25213ec6eb
scoobyboy <paulrichards20@msn.com>
2022-01-26 15:20:11 +0000

    k

--- tf_aks_mfests/main.tf
+++ tf_aks_mfests/main.tf
@@ -1,13 +1,20 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "=2.91.0"
     }
+     azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+     }
   }
-}
+} 
 
+# 2. Terraform Provider Block for AzureRM
 provider "azurerm" {
-  features {}
-}
+  features {
+  }
 
+}
